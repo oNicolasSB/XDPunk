e510df7005a6f6e8c4818b38305b7f99c4eea240
onicolassb <nicolasbassini@hotmail.com>
2025-12-18 21:10:35 -0300

    fix: proof of concept script adjusts

--- scripts/xdp_redirect.c
+++ scripts/xdp_redirect.c
@@ -1,17 +1,10 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
-#define INPUT_PORT 4
-#define OUTPUT_PORT 4
-
 SEC("xdp")
 int xdp_redirect_prog(struct xdp_md *ctx)
 {
-
-    if (OUTPUT_PORT == 0)
-        return XDP_PASS;
-
-    return bpf_redirect(OUTPUT_PORT, 0);
+    return bpf_redirect(4, 0);
 }
 
 char _license[] SEC("license") = "GPL";
